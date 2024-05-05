7b184dcb01a0b49706c2be54a085df9468830d75
ydrai <yoel.drai@gmail.com>
2024-05-05 18:34:30 +0300

    v3

--- app.py
+++ app.py
@@ -1,44 +1,114 @@
-# app.py
 import streamlit as st
 from aws_client import AWSClient
 from session_state import initialize_chat_history, add_message, display_chat
 from utils import set_page_layout, display_sidebar
+import base64
+import time
+
+def create_client():
+    """
+    Creates and stores an AWSClient instance in the session state if it doesn't already exist.
+    This function ensures that a client instance is available throughout the app's session.
+    """
+    if 'client' not in st.session_state:
+        st.session_state['client'] = AWSClient()
+
+def setup_sidebar():
+    """
+    Sets up the sidebar in the Streamlit interface with session details.
+    It displays the session ID for the current user's interaction session.
+    """
+    st.sidebar.title("Session Details")
+    session_id = str(st.session_state.client.session_id)[:15]
+    st.sidebar.write(f"Session ID: {session_id}")
+
+def handle_user_input():
+    """
+    Handles user input from the chat interface.
+    It sends the user's input to the AWS Lex service using the client and displays the response.
+    Errors are caught and displayed in the interface.
+    """
+    user_input = st.chat_input("Say something...")
+    if user_input:
+        add_message("user", user_input)
+        try:
+            bot_response = st.session_state.client.send_lex_message(user_input)
+            add_message("bot", bot_response)
+        except Exception as e:
+            st.error(f"An error occurred: {str(e)}")
+            add_message("bot", "Bot finished")
+
+def upload_section():
+    """
+    Manages the file upload section, allowing users to upload files when prompted by the chatbot.
+    It handles file uploads and displays the filename after successful upload.
+    """
+    def convert_uploaded_file_to_base64(uploaded_file):
+        """
+        Converts a Streamlit UploadedFile object to a Base64 string.
+        """
+        uploaded_file.seek(0)  # Reset file pointer to the start
+        file_data = uploaded_file.read()
+        return base64.b64encode(file_data).decode('utf-8')
+    
+    def display_progress_bar():
+        progress_bar = st.progress(0)
+        for percent_complete in range(100):
+            time.sleep(0.03)
+            progress_bar.progress(percent_complete + 1)
+
+        st.success('Full loading')
+    
+    text_to_find = "Le double de"
+    bot_message_found = any(
+        speaker == 'bot' and message.startswith(text_to_find)
+        for speaker, message in st.session_state.get('chat_history', [])
+    )
+
+    if bot_message_found:
+        uploaded_file = st.file_uploader("Upload an image...", type=["pdf", "jpg", "jpeg", "png", "tif", "tiff"])
+        if uploaded_file is not None:
+            base64_string = convert_uploaded_file_to_base64(uploaded_file)
+            
+            # Pose the question
+            question = "Do you confirm the information entered?"
+            
+            # Add a neutral default option
+            response = st.selectbox(question, ['Select an option', 'No ❌', 'Yes ✅'], index=0)
+
+            # Ensure the user makes a valid selection
+            if response == 'Select an option':
+                st.write("Please make a selection.")
+            
+            elif 'Yes' in response:
+                st.write("You have confirmed the information.")
+                display_progress_bar()
+                st.session_state['client'] = AWSClient(service_name='lambda')
+                entity_extraction = st.session_state.client.invoke_lambda('keyvalue_extraction', f'{{"image": "{base64_string}"}}')
+                st.json(entity_extraction)
+
+            else:
+                st.write("You have not confirmed the information.")
 
 def main():
-    set_page_layout()
-    display_sidebar()
+    """
+    Main function that serves as the entry point of the Streamlit application.
+    It orchestrates the layout setup, sidebar setup, chat initialization, and other UI components.
+    """
+    set_page_layout()  # Set the overall page layout settings
+    display_sidebar()  # Display static elements in the sidebar
+    create_client()  # Ensure an AWS client is created and stored
+    setup_sidebar()  # Set up dynamic elements in the sidebar
+    initialize_chat_history()  # Initialize chat history for new sessions
     
-    if 'client' not in st.session_state:
-        st.session_state.client = AWSClient()
-    
-    st.sidebar.title("Session Details")
-    st.sidebar.write(f"Session ID: {str(st.session_state.client.session_id)[:15]}")
-
-    initialize_chat_history()
-
     st.title(':grey[Claims - Tax Refund]')
     st.caption("If you paid taxes when you weren't supposed to, the chatbot will take care of everything 🙂")
     st.markdown('---')
 
-    user_input = st.chat_input("Say something...")
-
-    if user_input:
-        try:
-            add_message("user", user_input)
-            bot_response = st.session_state.client.send_message(user_input)
-            add_message("bot", bot_response)
-        except Exception as e:
-            st.write('Bot finished')
-            st.error(f"An error occurred: {str(e)}")
-
-    display_chat()
-
-    # Handling file upload example
-    if st.session_state.chat_history and st.session_state.chat_history[-1][1].startswith("Le double de"):
-        uploaded_file = st.file_uploader("Upload an image...", type=["jpg", "jpeg", "png"])
-        if uploaded_file is not None:
-            bytes_data = uploaded_file.read()
-            st.write("filename:", uploaded_file.name)
+    handle_user_input()  # Handle incoming user messages and responses
+    display_chat()  # Display the chat history and interactions
+    upload_section()  # Manage file uploads if prompted by the bot
 
 if __name__ == "__main__":
     main()
+
--- aws_client.py
+++ aws_client.py
@@ -1,36 +1,98 @@
-# aws_client.py
 from boto3.session import Session
 import streamlit as st
 import uuid
 
 class AWSClient:
-    def __init__(self, session_id=None):
-        # Utilisation des secrets directement
+    """
+    A class to create and manage AWS service clients using boto3, with the capability to interact with different services
+    based on initialization parameters.
+    
+    Attributes:
+        config (dict): Dictionary containing AWS credentials and configuration.
+        service_name (str): The name of the AWS service for which the client is created.
+        client: The boto3 client object for the specified AWS service.
+        session_id (str): A unique identifier for the session, used for services like Amazon Lex.
+    """
+
+    def __init__(self, service_name='lexv2-runtime', session_id=None):
+        """
+        Initializes the AWSClient with specific service details and credentials loaded from streamlit secrets.
+        
+        Args:
+            service_name (str): Specifies the AWS service for which the client will be created (default is 'lexv2-runtime').
+            session_id (str, optional): A unique identifier for interactions with services that require session management.
+                                       If not provided, a new UUID will be generated.
+        """
         self.config = {
-            'aws_access_key_id': st.secrets.aws_credentials.aws_access_key_id,
-            'aws_secret_access_key': st.secrets.aws_credentials.aws_secret_access_key,
-            'region_name': st.secrets.aws_credentials.region_name,
-            'bot_id': st.secrets.aws_credentials.bot_id,
-            'bot_alias_id': st.secrets.aws_credentials.bot_alias_id,
-            'locale_id': st.secrets.aws_credentials.locale_id
+            'aws_access_key_id': st.secrets["aws_credentials"]["aws_access_key_id"],
+            'aws_secret_access_key': st.secrets["aws_credentials"]["aws_secret_access_key"],
+            'region_name': st.secrets["aws_credentials"]["region_name"]
         }
-        self.client = self.create_client()
+        self.service_name = service_name
+        self.client = self.create_client(service_name)
         self.session_id = session_id if session_id else str(uuid.uuid4())
 
-    def create_client(self):
+    def create_client(self, service_name):
+        """
+        Creates a boto3 client for the specified AWS service.
+        
+        Args:
+            service_name (str): The name of the service for which the client should be created.
+        
+        Returns:
+            A boto3 service client object configured for the specified service.
+        """
         session = Session(
             aws_access_key_id=self.config['aws_access_key_id'],
             aws_secret_access_key=self.config['aws_secret_access_key'],
             region_name=self.config['region_name'],
         )
-        return session.client('lexv2-runtime')
+        return session.client(service_name)
 
-    def send_message(self, text):
+    def send_lex_message(self, text):
+        """
+        Sends a message to the Amazon Lex service and returns the response.
+        
+        Args:
+            text (str): The text message to send to the Amazon Lex bot.
+        
+        Returns:
+            str: The content of the message returned by Amazon Lex.
+        
+        Raises:
+            ValueError: If the service name is not 'lexv2-runtime'.
+        """
+        if self.service_name != 'lexv2-runtime':
+            raise ValueError("This method is only valid for Lex service.")
         response = self.client.recognize_text(
-            botId=self.config['bot_id'],
-            botAliasId=self.config['bot_alias_id'],
-            localeId=self.config['locale_id'],
-            sessionId=self.session_id,  # Utiliser l'UUID généré
+            botId=st.secrets.aws_credentials["bot_id"],
+            botAliasId=st.secrets.aws_credentials["bot_alias_id"],
+            localeId=st.secrets.aws_credentials["locale_id"],
+            sessionId=self.session_id,
             text=text
         )
         return response['messages'][0]['content']
+
+    def invoke_lambda(self, function_name, payload):
+        """
+        Invokes an AWS Lambda function with the specified function name and payload.
+        
+        Args:
+            function_name (str): The name of the Lambda function to invoke.
+            payload (str): The JSON payload string to send to the Lambda function.
+        
+        Returns:
+            bytes: The raw payload response from the Lambda function.
+        
+        Raises:
+            ValueError: If the service name is not 'lambda'.
+        """
+        if self.service_name != 'lambda':
+            raise ValueError("This method is only valid for Lambda service.")
+        response = self.client.invoke(
+            FunctionName=function_name,
+            InvocationType='RequestResponse',
+            Payload=payload
+        )
+        response_payload = response['Payload'].read()
+        return response_payload.decode()
